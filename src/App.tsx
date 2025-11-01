e9dbb8aec3d17bb2bd4a080723413dfd13ba1e54
yanislammari <yanis.lammari937@gmail.com>
2025-11-02 00:04:30 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import type React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
 import { Toaster } from "sonner";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
+import AppRoutes from "./AppRoutes";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <div>
       <Toaster position="bottom-left" theme="dark" richColors />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </BrowserRouter>
+      <AppRoutes />
+    </div>
   );
 }
 
---
+++ src/AppRoutes.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
